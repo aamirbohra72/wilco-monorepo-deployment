696b7f33829cac2aee2ab863a84a70a78d075d52
aamir-bohra <109149180+aamir-bohra@users.noreply.github.com>
2025-08-29 19:07:58 +0530

    port 8080

--- app/wilco-medusa-backend/index.js
+++ app/wilco-medusa-backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express(); 
 // const port = 3000; 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/health-check', (req, res) => {
   res.send('Hello, Wilco Medusa Backend!');
